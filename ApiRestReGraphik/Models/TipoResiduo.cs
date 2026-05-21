a5918a26b8827c788d570e08d278e665a0eb5d64
Luna <luna.beatrizalves@gmail.com>
2026-05-20 21:24:16 -0300

    Implementação dos atributos da classe TipoResiduo - Luna

--- ApiRestReGraphik/Models/TipoResiduo.cs
+++ ApiRestReGraphik/Models/TipoResiduo.cs
@@ -1,6 +1,13 @@
-﻿namespace ApiRestReGraphik.Models
+﻿using System.Text.Json.Serialization;
+
+namespace ApiRestReGraphik.Models
 {
     public class TipoResiduo
     {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("descricao_residuo")]
+        public required string DescricaoResiduo { get; set; }
     }
 }
