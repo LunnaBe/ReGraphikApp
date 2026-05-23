05bf17e07abe1f66b5b33e748927c22d9b741cac
kaioss99 <zaefpss@gmail.com>
2026-05-23 10:48:21 -0300

    adicionando mapa de pontos de coleta com Leaflet e Google Places API

--- ReGraphik/Views/Pages/PontosColetaPage.xaml.cs
+++ ReGraphik/Views/Pages/PontosColetaPage.xaml.cs
@@ -1,27 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace ReGraphik.Views.Pages
 {
-    /// <summary>
-    /// Lógica interna para PontosColetaPage.xaml
-    /// </summary>
-    public partial class PontosColetaPage : Window
+    public partial class PontosColetaPage : Page
     {
-        public PontosColetaPage()
-        {
-            InitializeComponent();
-        }
+        public PontosColetaPage() { InitializeComponent(); }
     }
 }
